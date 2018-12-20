e23770f95a40f6a15b1f79f1fc0903c3217f884d
willnilges <wdn5796@rit.edu>
2018-12-19 20:04:47 -0500

    I should make a different branch for each computer.

--- i3/polybar.sh
+++ i3/polybar.sh
@@ -6,6 +6,7 @@ killall xfce4-panel
 killall nm-applet
 
 # Wait until the processes have been shut down
+
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch polybar
@@ -14,3 +15,4 @@ polybar -c /home/will/.config/polybar/config/example example
 xfce4-panel --disable-wm-check
 
 nm-applet
+
