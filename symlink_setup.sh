671658a6d19ca1e4c6135717a8bd56a8fe36c6ab
Will Nilges <wdn5796@rit.edu>
2023-01-17 14:55:08 -0500

    Change a million things

--- symlink_setup.sh
+++ symlink_setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-ln -s ~/Code/dotfiles/.vimrc ~/.config/.vimrc
+ln -s ~/Code/dotfiles/.vimrc ~/.vimrc
 ln -s ~/Code/dotfiles/nvim/ ~/.config/nvim
 ln -s ~/Code/dotfiles/openbox ~/.config/openbox
 ln -s ~/Code/dotfiles/alacritty.yml ~/.config/alacritty.yml
 ln -s ~/Code/dotfiles/rofi ~/.config/rofi
+ln -s ~/Code/dotfiles/.zshrc ~/.zshrc
+ln -s ~/Code/dotfiles/.tmux.conf ~/.tmux.conf
--- touchpad.sh
+++ touchpad.sh
@@ -2,8 +2,9 @@
 ###Purpose: Toggle synaptic touchpad on/off
 ##Author: Fazle Arefin
 ##Reference: http://ubuntuforums.org/showthread.php?t=1536305
+#xinput set-prop "SynPS/2 Synaptics TouchPad" "Device Enabled" 0
 
-TOUCHPADID="10"   # change this id
+TOUCHPADID="SynPS/2 Synaptics TouchPad"   # change this id
 SYNSTATE=$(xinput list-props "$TOUCHPADID" | grep "Device Enabled" | grep -Eo '.$') 
 if [ $SYNSTATE = 0 ]; then 
 xinput set-prop "$TOUCHPADID" "Device Enabled" 1
