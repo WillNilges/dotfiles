f614d3f9c1cbe7e9a8ba24174ee232ecfb43ca7f
Willard Nilges <wdn5796@rit.edu>
2023-03-13 15:27:41 -0400

    Change indentation marker

--- nvim/init.lua
+++ nvim/init.lua
@@ -166,7 +166,7 @@ require('Comment').setup()
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
 require('indent_blankline').setup {
-  char = '┊',
+  char = '|',
   show_trailing_blankline_indent = false,
 }
 
