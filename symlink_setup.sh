b4ea421f6f289e3b3dfa57cca154a6e0dfadc52b
Will Nilges <wdn5796@rit.edu>
2023-06-23 06:48:44 -0400

    Add symlink setup for zsh theme

--- symlink_setup.sh
+++ symlink_setup.sh
@@ -7,3 +7,4 @@ ln -s ~/Code/dotfiles/alacritty.yml ~/.config/alacritty.yml
 ln -s ~/Code/dotfiles/rofi ~/.config/rofi
 ln -s ~/Code/dotfiles/.zshrc ~/.zshrc
 ln -s ~/Code/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/Code/dotfiles/wilnil-lowkey.zsh-theme ~/.oh-my-zsh/custom/themes/wilnil-lowkey.zsh-theme
