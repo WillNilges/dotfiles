455261c5e9f28d12300e27926f661571499e5970
Willard Nilges <willard.nilges@datadoghq.com>
2023-11-27 00:45:00 -0500

    Fix placement of configs

--- nvim/init.lua
+++ nvim/init.lua
@@ -628,19 +628,5 @@ vim.cmd [[autocmd FileChangedShellPost *
 
 vim.cmd[[autocmd CursorHold * lua vim.diagnostic.open_float({scope="line"})]]
 
--- The line beneath this is called `modeline`. See `:help modeline`
+-- This is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-
--- Triger `autoread` when files changes on disk
--- https://unix.stackexchange.com/questions/149209/refresh-changed-content-of-file-opened-in-vim/383044#383044
--- https://vi.stackexchange.com/questions/13692/prevent-focusgained-autocmd-running-in-command-line-editing-mode
-vim.cmd [[autocmd FocusGained,BufEnter,CursorHold,CursorHoldI *
-          \ if mode() !~ '\v(c|r.?|!|t)' && getcmdwintype() == '' | checktime | endif]]
-
--- Notification after file change
--- https://vi.stackexchange.com/questions/13091/autocmd-event-for-autoread
-vim.cmd [[autocmd FileChangedShellPost *
-  \ echohl WarningMsg | echo "File changed on disk. Buffer reloaded." | echohl None]]
-
-
-vim.cmd[[autocmd CursorHold * lua vim.diagnostic.open_float({scope="line"})]]
