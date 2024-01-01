7e194233919638a55bd0cb8899a7971e87c356d8
Will Nilges <wdn5796@rit.edu>
2024-01-01 10:16:35 -0500

    fuck fuck fuck

--- nvim/init.lua
+++ nvim/init.lua
@@ -200,6 +200,22 @@ require('lazy').setup({
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    opts = {
+      filesystem = {
+        filtered_items = {
+    visible = true,
+    show_hidden_count = true,
+    hide_dotfiles = false,
+    hide_gitignored = false,
+    hide_by_name = {
+      -- '.git',
+      -- '.DS_Store',
+      -- 'thumbs.db',
+    },
+    never_show = {},
+        },
+      }
     }
   },
   {
