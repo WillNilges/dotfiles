9d5dea12ba1874668cddcc855a32d49bd818b00e
Willard Nilges <willard.nilges@datadoghq.com>
2025-04-01 10:51:28 -0400

    update lads

--- nvim/init.lua
+++ nvim/init.lua
@@ -213,6 +213,8 @@ require('lazy').setup({
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   -- { import = 'custom.plugins' },
+
+  -- Willard's Plugins
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
@@ -238,6 +240,21 @@ require('lazy').setup({
   {
     "RRethy/vim-illuminate",
   },
+  {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+},
+
 }, {})
 
 -- [[ Setting options ]]
