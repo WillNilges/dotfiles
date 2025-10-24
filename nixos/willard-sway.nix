62d758d72cace8b62dc47351c898463ff777f2f7
Willard Nilges <willardnilges@gmail.com>
2025-10-23 20:37:08 -0400

    waybar config

--- nixos/willard-sway.nix
+++ nixos/willard-sway.nix
@@ -5,12 +5,12 @@
     mako # notification system developed by swaywm maintainer
     networkmanagerapplet
     slurp # screenshot functionality
+    swayidle
+    swaylock
+    waybar
     wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
   ];
 
-  # Enable the gnome-keyring secrets vault. 
-  # Will be exposed through DBus to programs willing to store secrets.
-  services.gnome.gnome-keyring.enable = true;
 
   # enable sway window manager
   programs.sway = {
@@ -18,16 +18,8 @@
     wrapperFeatures.gtk = true;
   };
 
-
-
-  # Sway?
-  programs.sway.enable = true;
-  programs.sway.wrapperFeatures.gtk = true;
+  # Enable the gnome-keyring secrets vault. 
+  # Will be exposed through DBus to programs willing to store secrets.
   services.gnome.gnome-keyring.enable = true;
   services.blueman.enable = true;
-  #programs.networkmanagerapplet.enable = true;
-
-  
-
-
 }
