65085b6a76fbad66b2fec917a6f381ed08d66a13
Willard Nilges <willard.nilges@datadoghq.com>
2025-12-08 02:20:18 -0500

    Sigh... updates

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 # Setup script for datadog workspaces.
 
+# Band-Aid fix my signing key
+git config --global user.signingKey "key::ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA21sliaNUYxI4KeUqJaDkJD2fZ+six7gF/U2BEBCC2i"
+
 # Compile and install neovim from source (Ubuntu's version of Neovim is old)
 # This will also grab my nvim config
 bash $HOME/dotfiles/scripts/compile_neovim.sh
--- scripts/compile_neovim.sh
+++ scripts/compile_neovim.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-git clone git@github.com:neovim/neovim || echo 'already cloned nvim'
+git clone https://github.com/neovim/neovim || echo 'already cloned nvim'
 cd neovim
 git checkout stable
 make CMAKE_BUILD_TYPE=RelWithDebInfo
