de026062fa8408111e1a0742fba92018d4ff5165
Willard Nilges <wdn5796@rit.edu>
2026-03-26 18:44:54 -0400

    add r2modman

--- nixos/modules/steam.nix
+++ nixos/modules/steam.nix
@@ -7,4 +7,9 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
+
+  environment.systemPackages = with pkgs; [
+    r2modman
+  ];
+
 }
