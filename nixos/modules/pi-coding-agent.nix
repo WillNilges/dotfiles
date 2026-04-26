9f929ff801793dae5543f43bc5ffc1c256f63ecd
Willard Nilges <willard.nilges@datadoghq.com>
2026-04-26 01:39:16 -0400

    try this

--- nixos/modules/pi-coding-agent.nix
+++ nixos/modules/pi-coding-agent.nix
@@ -1,25 +1,38 @@
 { config, pkgs, lib, ... }:
 
 let
-  pi-coding-agent = pkgs.buildNpmPackage rec {
+  pi-coding-agent = pkgs.buildNpmPackage {
     pname = "pi-coding-agent";
-    version = "1.0.0";
+    version = "0.70.2";
 
-    src = pkgs.fetchNpmPackage {
-      name = "@mariozechner/pi-coding-agent";
-      version = "latest";
+    src = pkgs.fetchurl {
+      url = "https://registry.npmjs.org/@mariozechner/pi-coding-agent/-/pi-coding-agent-0.70.2.tgz";
+      hash = lib.fakeHash;
     };
 
     npmDepsHash = lib.fakeHash;
 
-    buildPhase = ''
-      npm run build
-    '';
+    # Package is already built in the tarball
+    dontNpmBuild = true;
 
     installPhase = ''
+      runHook preInstall
+      
+      mkdir -p $out/lib/node_modules/@mariozechner/pi-coding-agent
+      cp -r . $out/lib/node_modules/@mariozechner/pi-coding-agent/
+      
       mkdir -p $out/bin
-      npm install -g --prefix $out .
+      ln -s $out/lib/node_modules/@mariozechner/pi-coding-agent/dist/cli.js $out/bin/pi
+      
+      runHook postInstall
     '';
+
+    meta = with lib; {
+      description = "CLI coding agent harness";
+      homepage = "https://github.com/MarioZechner/pi-coding-agent";
+      license = licenses.mit;
+      mainProgram = "pi";
+    };
   };
 in
 {
